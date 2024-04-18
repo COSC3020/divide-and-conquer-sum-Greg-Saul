62b1b2b9164e8a1ee8f15efe514ff6952560c165
Greg-Saul <123204473+Greg-Saul@users.noreply.github.com>
2024-04-17 18:11:47 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,5 +1,4 @@
 function divideAndConquerSum(array) {
-    let sum = 0;
     switch(array.length){
         case 0:
             return 0;
@@ -11,29 +10,14 @@ function divideAndConquerSum(array) {
             return array[0] + array[1];
             break;
         default:
-            sum = helper(array);
-            return sum;
-    }  
-}
-
-function helper(arr){
-    switch(arr.length){
-        // case 1:
-        //     return arr[0];
-        //     break;
-        // case 2:
-        //     return arr[0] + arr[1];
-        //     break;
-        default:
-            let firstThird = Math.floor(arr.length / 3);
+            let firstThird = Math.floor(array.length / 3);
             let secondThird = firstThird * 2;
 
-            let first = arr.slice(0, firstThird);
-            let second = arr.slice(firstThird, secondThird);
-            let third = arr.slice(secondThird, arr.length)
+            let first = array.slice(0, firstThird);
+            let second = array.slice(firstThird, secondThird);
+            let third = array.slice(secondThird, array.length)
 
-            let sum = helper(first) + helper(second) + helper(third);
+            let sum = divideAndConquerSum(first) + divideAndConquerSum(second) + divideAndConquerSum(third);
             return sum;
     }  
-    
 }
